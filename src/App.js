a69b5201a385c1b27b00c498410411906229cd96
EmilyCook321 <emilyacook16@gmail.com>
2020-08-14 14:49:00 +0200

    Add List Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./App.css";
-import Logo from "./assets/WHISKEYMANIACS.png";
+import logoSrc from "./assets/WHISKEYMANIACS.png";
+import List from "./components/List";
+import ListItems from "./components/ListItems";
+import ListItemImage from "./components/ListItemImage";
+import ListItemText from "./components/ListItemText";
 
 function App() {
   return (
     <div className="app">
       <header className="header">
         <h1>Whiskey World</h1>
-        <img className="app__logo" src={Logo}></img>{" "}
-        <input className="input" placeholder="Get your stuff..." />
-        <alt>"Whisky World App"</alt>
+        <img className="app__logo" src={logoSrc} alt="Whisky World App" />
+        <input className="input" placeholder="Get your whiskey..." />
       </header>
-      <main className="main"></main>
+      <main className="main">
+        <List>
+          <ListItems>
+            <ListItemImage src={"logoSrc"} alt="Whiskeymaniacs logo" />
+            <ListItemText title={""} description={""}></ListItemText>
+          </ListItems>
+        </List>
+      </main>
       <footer>Footer</footer>
     </div>
   );
---
+++ src/components/WhiskeyList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from './List';
+import List from './ListItems';
+import List from './ListItemImage';
+import List from "./ListItemText";
+
+function WhiskeyList({children}) {
+    return (
+
+    );
+}
+
+export default WhiskeyList;
